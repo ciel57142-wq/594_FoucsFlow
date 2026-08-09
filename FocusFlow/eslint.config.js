ff27d5bf476da972a7d89e00db71fd45d5f07671
Ciel <ciel57142@gmail.com>
2026-08-09 16:32:31 -0500

    fix(domain): inline @typescript-eslint recommended rules for ESLint flat config

--- FocusFlow/eslint.config.js
+++ FocusFlow/eslint.config.js
@@ -5,7 +5,6 @@ const reactHooksPlugin = require('eslint-plugin-react-hooks');
 
 module.exports = [
   configs.recommended,
-  typescriptPlugin.configs.recommended,
   {
     ignores: ['**/eslint.config.js', '**/babel.config.js', '**/*.config.js'],
   },
@@ -35,9 +34,29 @@ module.exports = [
       'react-hooks': reactHooksPlugin,
     },
     rules: {
+      '@typescript-eslint/ban-ts-comment': 'error',
+      'no-array-constructor': 'off',
+      '@typescript-eslint/no-array-constructor': 'error',
+      '@typescript-eslint/no-duplicate-enum-values': 'error',
+      '@typescript-eslint/no-empty-object-type': 'error',
+      '@typescript-eslint/no-explicit-any': 'error',
+      '@typescript-eslint/no-extra-non-null-assertion': 'error',
+      '@typescript-eslint/no-misused-new': 'error',
+      '@typescript-eslint/no-namespace': 'error',
+      '@typescript-eslint/no-non-null-asserted-optional-chain': 'error',
+      '@typescript-eslint/no-require-imports': 'error',
+      '@typescript-eslint/no-this-alias': 'error',
+      '@typescript-eslint/no-unnecessary-type-constraint': 'error',
+      '@typescript-eslint/no-unsafe-declaration-merging': 'error',
+      '@typescript-eslint/no-unsafe-function-type': 'error',
+      'no-unused-expressions': 'off',
+      '@typescript-eslint/no-unused-expressions': 'error',
       'no-unused-vars': 'off',
       '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
-      '@typescript-eslint/no-explicit-any': 'error',
+      '@typescript-eslint/no-wrapper-object-types': 'error',
+      '@typescript-eslint/prefer-as-const': 'error',
+      '@typescript-eslint/prefer-namespace-keyword': 'error',
+      '@typescript-eslint/triple-slash-reference': 'error',
     },
   },
 ];
